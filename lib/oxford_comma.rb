7ebc76cf0cc36653e0e9b6b1c788439de66b84db
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-12 04:13:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    return array.join
+  elsif array.count == 2
+    return array.join(" and ")
+  else
+    last_word = array.pop
+    final_str = array.join(", ")
+    final_str += ", and " + last_word
+    return final_str
+  end
 end
